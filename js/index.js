b3e7cd1ad98d7ed3d5d543ab4feea9aac17f6f61
Shamsiddinxon <nosirovshamsiddinxon@gmail.com>
2021-12-15 00:59:07 +0500

    page build

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const elHamburgerBtn = document.querySelector (".item__bottom") ;
-const elHeader = document.querySelector (".item") ;
+const elHeader = document.querySelector (".directors__item") ;
 
 elHamburgerBtn.addEventListener ('click', ()=> {
     elHeader.classList.toggle("header__open") ;
