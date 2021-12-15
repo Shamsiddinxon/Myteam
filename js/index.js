24d620ab41f3f5e8281c2a6ca2c97624c39bee7d
Shamsiddinxon <nosirovshamsiddinxon@gmail.com>
2021-12-15 15:31:57 +0500

    feat: add basic page layout

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
-const elHamburgerBtn = document.querySelector (".item__bottom") ;
-const elHeader = document.querySelector (".directors__item") ;
+const elHamburgerBtn = document.querySelector(".header__hamburger") ;
+// const elHamburgerBtn = document.querySelector (".item__bottom") ;
+// const elHeader = document.querySelector (".directors__item") ;
+const elHeader = document.querySelector(".header") ;
 
-elHamburgerBtn.addEventListener ('click', ()=> {
-    elHeader.classList.toggle("header__open") ;
+
+
+elHamburgerBtn.addEventListener('click', ()=> {
+    elHeader.classList.toggle("header--open");
 })
